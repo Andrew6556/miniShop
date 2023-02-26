7aaf6be5ddc485f644ccc21c7655cb305f36e6d3
Andrey Kozlov <andrew08552gmail.com>
2023-02-26 23:17:40 +0300

    purchase goods successfully completed

--- js/script.js
+++ js/script.js
@@ -117,7 +117,6 @@ let num_buy    = 0,
 
 
 
-// document.querySelector(".goods__btn-hover").addEventListener("click", increase_number_purchases);
 
 function add_div(){
     localStorage.setItem("product", list_goods);
@@ -146,37 +145,46 @@ function pass_class(div_product){
 
 
 
-// let goodsDom = document.querySelector(".goods");
+let goodsDom = document.querySelector(".goods");
     
 
-// for (let product of goods){
-//     let productDiv = document.createElement('div');
-//     productDiv.classList.add("goods__card");
-
-//     productDiv.innerHTML = `<h1 class="goods__title">${product.name}</h1>
-//                         <div class="goods__inner">
-//                             <p class="goods__text">
-//                                 ${product.description}
-//                             </p>
-                            
-//                             <div class="goods__wrap">
-//                                 <div class="goods__item">
-//                                     <p class="goods__item-title">Ширина экрана</h3>
-//                                     <p class="goods__passenger goods__item-text">${product.screen}</p>
-//                                 </div>
-//                                 <div class="goods__item">
-//                                     <h3 class="goods__item-title">Память</h3>
-//                                     <p class="goods__passport goods__item-text">${product.memory}</p>
-//                                 </div>
-//                             </div>
-//                             <div class="goods__price">
-//                                 Цена: <span class="goods__price-text">${product.price}</span>
-//                             </div>
-//                         </div>
-//                     <div class="goods__index">1</div>`;
-//     goodsDom.appendChild(productDiv);
-//     // break
-// }
+for (let product of goods){
+    let productDiv = document.createElement('div');
+    productDiv.classList.add("goods__block");
+    productDiv.innerHTML = `<div class="goods__card" onclick="pass_class(this),increase_number_purchases()">
+                            <h1 class="goods__title">${product.name}</h1>
+                            <div class="goods__inner">
+                                <div class="goods__wrap">
+                                    <div class="goods__item">
+                                        <p class="goods__item-title">Страна</h3>
+                                        <p class="goods__passenger goods__item-text">${product.country}</p>
+                                    </div>
+                                    <div class="goods__item">
+                                        <h3 class="goods__item-title">Гарантия</h3>
+                                        <p class="goods__passport goods__item-text">${product.guarantee}</p>
+                                    </div>
+                                </div>
+                                <div class="goods__wrap">
+                                    <div class="goods__item">
+                                        <p class="goods__item-title">Ширина экрана</h3>
+                                        <p class="goods__passenger goods__item-text">${product.screen}</p>
+                                    </div>
+                                    <div class="goods__item">
+                                        <h3 class="goods__item-title">Память</h3>
+                                        <p class="goods__passport goods__item-text">${product.memory}</p>
+                                    </div>
+                                </div>
+                                <div class="goods__price">
+                                    Цена: <span class="goods__price-text">${product.price}</span>
+                                </div>
+                            </div>
+                            <div class="goods__hover-buy">
+                                <button class="goods__btn-hover">Купить</button>
+                            </div>
+                        </div>`;
+    goodsDom.appendChild(productDiv);
+    // break
+}
 
 
 
