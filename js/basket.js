04c167f7de7d71b699bce191db6779abca7e1b9b
Andrey Kozlov <andrew08552gmail.com>
2023-02-28 07:25:37 +0300

    made a minimalistic adaptive

--- js/basket.js
+++ js/basket.js
@@ -5,7 +5,6 @@ let div_product = localStorage.getItem("product"),
 
 div_basket.innerHTML = div_product;
 
-// document.querySelector(".header").style.display = 'none';
 document.querySelector(".navbar__user-authorized").style.display = 'flex';
 document.querySelector(".navbar__username").innerText = localStorage.getItem("username");
 console.log(1);
