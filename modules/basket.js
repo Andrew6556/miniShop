fc573b5658f3d13464e4c9df33d2f512b32bb09b
Andrey Kozlov <andrew08552gmail.com>
2023-03-15 04:39:55 +0300

    cart class ready

--- modules/basket.js
+++ modules/basket.js
@@ -2,6 +2,16 @@
 
 export default class basket{
     constructor(){
-        
+        this.wrapper = document.createElement("div")
+        this.wrapper.classList.add("basket")
+
+        this.wrapperTitle = document.createElement("div");
+        this.wrapperTitle.classList.add("basket__title")
+
+        this.wrapperContent = document.createElement("div");
+        this.wrapperContent.classList.add("basket__content")
+
+        this.wrapper.appendChild(this.wrapperTitle)
+        this.wrapper.appendChild(this.wrapperContent)
     }
 }
